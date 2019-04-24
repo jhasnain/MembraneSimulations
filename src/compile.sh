de4d862593e5b4b694e423ce5d9cd2360a49715d
jhasnain <jaffarenator@gmail.com>
2019-04-24 16:23:12 -0700

    first test, changed compile -g ... to compile with out the g

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
-gcc -g -Wall -Wformat-truncation=0 -O3 main.c mesh.c effectiveparticlepotential.c meshbiasfuncs.c utilities.c initialize.c parseparameters.c errorcheck.c -lm -lgsl -lgslcblas -o ../LatticeMembrane;
+gcc -Wall -Wformat-truncation=0 -O3 main.c mesh.c effectiveparticlepotential.c meshbiasfuncs.c utilities.c initialize.c parseparameters.c errorcheck.c -lm -lgsl -lgslcblas -o ../LatticeMembrane;
 #  -no-pie -pg
 #-g
 
